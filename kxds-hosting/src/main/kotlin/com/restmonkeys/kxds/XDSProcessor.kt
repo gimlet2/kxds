6010cef3b6c642c1321e09b694b6ee4033126618
Andrei Chernyshev <andrey.chernishov@gmail.com>
2025-01-22 22:58:16 +0100

    elements with type

--- kxds-hosting/src/main/kotlin/com/restmonkeys/kxds/XDSProcessor.kt
+++ kxds-hosting/src/main/kotlin/com/restmonkeys/kxds/XDSProcessor.kt
@@ -11,8 +11,10 @@ import com.google.devtools.ksp.symbol.KSAnnotated
 import com.squareup.kotlinpoet.*
 import jakarta.xml.bind.JAXBContext
 import jakarta.xml.bind.JAXBElement
+import org.w3._2001.xmlschema.ComplexType
 import org.w3._2001.xmlschema.LocalElement
 import org.w3._2001.xmlschema.Schema
+import org.w3._2001.xmlschema.TopLevelComplexType
 import org.w3._2001.xmlschema.TopLevelElement
 import java.io.File
 import javax.xml.namespace.QName
@@ -24,6 +26,7 @@ class XDSProcessor(
     val logger: KSPLogger,
     val options: Map<String, String>
 ) : SymbolProcessor {
+
     override fun process(resolver: Resolver): List<KSAnnotated> {
         val rootPath = options["path"] ?: error("path option is required")
         resolver.getSymbolsWithAnnotation("com.restmonkeys.kxds.XDSProcessorAnnotation")
@@ -40,49 +43,76 @@ class XDSProcessor(
         return emptyList()
     }
 
-    fun generate(schema: String){
+    fun generate(schema: String) {
         val context = JAXBContext.newInstance(Schema::class.java.packageName, Schema::class.java.classLoader)
         val result = context.createUnmarshaller()
             .unmarshal(schema.trim().byteInputStream())
         result as Schema
         result.simpleTypeOrComplexTypeOrGroup.forEach {
             when (it) {
+                is TopLevelComplexType -> {
+                    logger.warn(it.name)
+                    toDataClass(it).toCode()
+                }
+
                 is TopLevelElement -> {
                     logger.warn(it.name)
-                    val k = TypeSpec.classBuilder(it.name)
-                        .addModifiers(KModifier.DATA).also { t ->
-                            t.primaryConstructor(FunSpec.constructorBuilder().also { c ->
-                                logger.warn(it.name)
-                                it.complexType.sequence.particle.forEach {
-                                    when (val p = it) {
-                                        is JAXBElement<*> -> {
-                                            when (val e = p.value) {
-                                                is LocalElement -> {
-
-                                                    c.addParameter(e.name, getType(e.type)).build()
-                                                    t.addProperty(
-                                                        PropertySpec.builder(e.name, getType(e.type))
-                                                            .initializer(e.name)
-                                                            .build()
-                                                    )
-                                                    println(e.name + " " + e.type)
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-
-                            }.build())
-                        }.build()
-                    codeGenerator.createNewFile(Dependencies.ALL_FILES, "", it.name!!).bufferedWriter().use {
-                        FileSpec.builder("", k.name!!).addType(k)
-                            .build().writeTo(it)
+                    it.complexType?.let { complexType ->
+                        toDataClass(complexType).toCode()
                     }
-
                 }
             }
         }
     }
+
+    fun TypeSpec.toCode() {
+        codeGenerator.createNewFile(Dependencies.ALL_FILES, "", name!!).bufferedWriter().use {
+            FileSpec.builder("", name!!).addType(this)
+                .build().writeTo(it)
+        }
+    }
+
+    fun toDataClass(complexType: ComplexType): TypeSpec {
+        return TypeSpec.classBuilder(complexType.name)
+            .addModifiers(KModifier.DATA).also { t ->
+                t.primaryConstructor(FunSpec.constructorBuilder().also { c ->
+                    complexType.choice?.particle?.forEach {
+                        when (val p = it) {
+                            is JAXBElement<*> -> {
+                                when (val e = p.value) {
+                                    is LocalElement -> {
+                                        c.addParameter(e.name, getType(e.type)).build()
+                                        t.addProperty(
+                                            PropertySpec.builder(e.name, getType(e.type))
+                                                .initializer(e.name)
+                                                .build()
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    complexType.sequence?.particle?.forEach {
+                        when (val p = it) {
+                            is JAXBElement<*> -> {
+                                when (val e = p.value) {
+                                    is LocalElement -> {
+                                        c.addParameter(e.name, getType(e.type)).build()
+                                        t.addProperty(
+                                            PropertySpec.builder(e.name, getType(e.type))
+                                                .initializer(e.name)
+                                                .build()
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }.build())
+            }.build()
+
+    }
+
     fun getType(t: QName): KClass<*> {
         return when (t.localPart) {
             "string" -> String::class
